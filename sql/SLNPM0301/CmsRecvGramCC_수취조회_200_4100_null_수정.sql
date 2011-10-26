ea6354c47cd2d1e178552805bf4995ebbed6647c
Alex Yang <movingmt@gmail.com>
2011-10-27 08:38:40 +0900

    수취조회 - 대출번호 조회 쿼리 수정  - 기존 조인 쿼리 없앰  - 단순한 단일테이블 쿼리로 수정

--- sql/SLNPM0301/CmsRecvGramCC_수취조회_200_4100_null_수정.sql
+++ sql/SLNPM0301/CmsRecvGramCC_수취조회_200_4100_null_수정.sql
@@ -1,4 +1,48 @@
 
+--------------------------------------------------
+SELECT  NVL(LOAN_NO,' ')       LOAN_NO
+       ,NVL(LOAN_SEQ,'01')     LOAN_SEQ
+       ,NVL(MNG_DEPT_CD, ' ')  MNG_DEPT_CD
+      FROM  AUSER.ALOT_LOAN_BASE
+     WHERE
+       IMG_ACNT_NO      = ?                      /* :입금내역.계좌번호  */
+       AND IMG_ACNT_BANK_CD = '0'||?             /* :입금내역.은행_코드 */
+       AND LOAN_LAST_FG     = '1'
+       ;
+--------------------------------------------------
+SELECT  NVL(LOAN_NO,' ')       LOAN_NO
+       ,NVL(LOAN_SEQ,'01')     LOAN_SEQ
+       ,NVL(MNG_DEPT_CD, ' ')  MNG_DEPT_CD
+      FROM  AUSER.ALOT_LOAN_BASE
+     WHERE
+       IMG_ACNT_NO          = '56201551042002'  /* :입금내역.계좌번호  */
+       AND IMG_ACNT_BANK_CD = '026'             /* :입금내역.은행_코드 */
+       AND LOAN_LAST_FG     = '1'
+;
+--------------------------------------------------
+-- 마지막 코드 여러건 찾기
+--------------------------------------------------
+select loan_no, count(*)
+from auser.alot_loan_base
+where loan_last_fg = '1'
+group by loan_no
+having count(*) > 1
+;
+
+select loan_no, loan_seq, loan_last_fg, loan_dt, end_dt
+from auser.alot_loan_base
+where
+    loan_no in (
+        select loan_no
+        from auser.alot_loan_base
+        where loan_last_fg = '1'
+        group by loan_no
+        having count(*) > 1
+    )
+    and loan_last_fg = '1'
+order by loan_no, loan_seq
+;
+--------------------------------------------------
 
 select * from (
 SELECT  CASE WHEN SUBSTR(B.LOAN_STAT_CD,1,1) = '2' 
@@ -20,8 +64,15 @@ SELECT  CASE WHEN SUBSTR(B.LOAN_STAT_CD,1,1) = '2'
 	--  AND LOAN_NO is not null    
 ;
 
+select * from buser.bvat_uncnfm_base
+;
+
+select LOAN_NO, LOAN_SEQ, LOAN_STAT_CD, MNG_DEPT_CD from AUSER.ALOT_LOAN_BASE 
+where  TRIM(IMG_ACNT_NO)  = '56201551042002' --'56201555007422'         /* :입금내역.계좌번호   */
+;
+
 select * from AUSER.ALOT_LOAN_BASE 
-where  TRIM(IMG_ACNT_NO)  = '56201555007422'         /* :입금내역.계좌번호   */
+where  TRIM(IMG_ACNT_NO)  = '56201551042002' --'56201555007422'         /* :입금내역.계좌번호   */
 ;
 
 select rowid, a.* from AUSER.ALOT_LOAN_BASE a
@@ -106,19 +157,74 @@ group by IMG_ACNT_BANK_CD
 ;
 
 ---------------------------------------------
-SELECT  CASE WHEN SUBSTR(B.LOAN_STAT_CD,1,1) = '2' 
-    THEN LOAN_NO                            
-    WHEN SUBSTR(A.LOAN_STAT_CD,1,1) = '3'   
-      THEN '344'                            
-      ELSE null  END    AS LOAN_NO                  
-   , A.LOAN_STAT_CD, B.LOAN_STAT_CD
+SELECT
+    CASE
+        WHEN SUBSTR(B.LOAN_STAT_CD,1,1) = '2' THEN LOAN_NO                            
+        WHEN SUBSTR(A.LOAN_STAT_CD,1,1) = '3' THEN '344'                            
+        ELSE null
+    END AS LOAN_NO                  
+--   , A.LOAN_STAT_CD, B.LOAN_STAT_CD
    ,NVL(B.LOAN_SEQ,'01')               LOAN_SEQ    
    ,NVL(B.MNG_DEPT_CD, A.MNG_DEPT_CD)  MNG_DEPT_CD  
-  FROM  BUSER.BVAT_UNCNFM_BASE A        
-             ,AUSER.ALOT_LOAN_BASE    B       
+FROM  BUSER.BVAT_UNCNFM_BASE A        
+    ,AUSER.ALOT_LOAN_BASE    B       
      WHERE DIV_CD           = 1         /* '1'              */    
-       AND TRIM(IMG_ACNT_NO)  = '56201551042002'         /* :입금내역.계좌번호  운영 */  
+       AND TRIM(IMG_ACNT_NO)  = '56201551042002'         /* :입금내역.계좌번호  운영 / 개발*/  
 --       AND TRIM(IMG_ACNT_NO)  = '56201554091224'       /* :입금내역.계좌번호  개발  */  
        --AND IMG_ACNT_BANK_CD = '0'||?    /* :입금내역.은행_코드 */	  
 ;
 ----------------------------------------------
+select
+    LOAN_NO
+    , LOAN_SEQ
+    , MNG_DEPT_CD
+    , LOAN_STAT_CD
+    , IMG_ACNT_BANK_CD
+--    , a.*
+from AUSER.ALOT_LOAN_BASE a
+where
+    TRIM(IMG_ACNT_NO)  = '56201551042002' --'56201555007422'         /* :입금내역.계좌번호   */
+;
+----------------------------------------------
+select
+    LOAN_NO
+    , LOAN_SEQ
+    , MNG_DEPT_CD
+    , LOAN_STAT_CD
+    , IMG_ACNT_BANK_CD
+--    , a.*
+from AUSER.ALOT_LOAN_BASE a;
+----------------------------------------------
+-- '026' count 143311
+-- '088' count 324 - 사용하지 않는 코드 [예전것임 / 종료된 계좌 / 최종 종료일 (2010-03-09)]
+----------------------------------------------
+select IMG_ACNT_BANK_CD, count(*)
+from AUSER.ALOT_LOAN_BASE a
+group by IMG_ACNT_BANK_CD
+;
+
+select loan_stat_cd, loan_dt, end_dt, a.*
+from AUSER.ALOT_LOAN_BASE a
+where
+    IMG_ACNT_BANK_CD = '088'
+--    AND LOAN_SEQ > '01'
+--    AND LOAN_LAST_FG = '1'
+;
+
+select LOAN_NO, COUNT(*)
+from AUSER.ALOT_LOAN_BASE a
+where
+    IMG_ACNT_BANK_CD = '088'
+    AND LOAN_SEQ > '01'
+--    AND LOAN_LAST_FG = '1'
+GROUP BY LOAN_NO
+HAVING COUNT(*) > 1
+;
+------------------------------------------------
+select * from AUSER.ALOT_LOAN_IMG_ACNT_DESC
+;
+
+select IMG_ACNT_BANK_CD
+from AUSER.ALOT_LOAN_IMG_ACNT_DESC
+group by IMG_ACNT_BANK_CD
+
