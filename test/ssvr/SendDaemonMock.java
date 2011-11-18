8decb307f1335dbf32cd3e8d66d8a9b2c871ce91
Alex Yang <movingmt@gmail.com>
2011-11-18 20:30:55 +0900

    Client subclass 추가

--- test/ssvr/SendDaemonMock.java
+++ test/ssvr/SendDaemonMock.java
@@ -9,19 +9,16 @@ public class SendDaemonMock extends SendDaemon {
 
 	public SendDaemonMock() throws Exception {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	class ClientMock extends SendDaemon.Client {
 
 		ClientMock(Socket con) throws Exception {
 			super(con);
-			// TODO Auto-generated constructor stub
 		}
 		
 		@Override
 		public String rcvMsg() throws IOException {
-			// TODO Auto-generated method stub
 			return super.rcvMsg();
 		}
 	}
