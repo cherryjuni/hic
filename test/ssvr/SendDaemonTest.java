fce899b9974f14fc1d14445e3e0519518de0e4ce
Alex Yang <movingmt@gmail.com>
2011-11-24 16:03:32 +0900

    SD 테스트 케이스 보강

--- test/ssvr/SendDaemonTest.java
+++ test/ssvr/SendDaemonTest.java
@@ -6,11 +6,7 @@ import static org.junit.Assert.fail;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -18,10 +14,22 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import util.StringUtil;
+import com.cabis.fw.util.gram.GramExecutor;
 
 public class SendDaemonTest {
+	// KIBNET 대외계관련 선언
+	// 1. GRAM_CD 로 채널 정보 가져오기
+	//    TABLE - GUSER.GSCT_GRAM_BASE
+	//      DEV  - CHNL04
+	//      REAL - CHNL14
+	// 2. 1번에서 선택된 채널[CHNL04]에서 IP / PORT 가져오기
+	//    TABLE - GUSER.GSCT_GRAM_CHNL
+	//      DEV  - IP:172.20.101.20 PORT:9130
+	//      REAL - IP:172.20.101.20 PORT:9530
+	private final String GRAM_CD = "KB0200";
+
 	private static final int 상수_지급이체테스트 = 0;
+	private static final int 상수_지급이체테스트_20111124_01 = 1;
 //	private static final int 상수_예상전문_지급이체01_수취조회 = 1;
 //	private static final int 상수_예상전문_지급이체02_지급이체 = 2;
 	
@@ -55,17 +63,17 @@ public class SendDaemonTest {
 	// 가상계좌전송 테스트시 기존 데이터 삭제 필요
 	// 작업후, 테이블에 추가 됨
 	// delete from buser.bvat_cm_iamt_desc where tr_org_cd = 'C1004' and tr_no = '201111030209662' and tr_dt = '2011-03'
-	String[] sd01_지급이체              = { "KB0200"+"06040600000920032902000052003290220032902 30200110000020111116181048007732920000009880                                                                                                                                                                                                      000000              0000003000000 000000000000000000000000000000008800110083688780    홍성희              0000008800100024180210            하이캐피탈          0000000000              00              00              00              00              00               00000                                                  "
-                                          , ""
+	String[] sd01_지급이체              = { "06040600000920032902000052003290220032902 30200110000020111123100249007828220000009880                                                                                                                                                                                                      000000              0000004000000 000000000000000000000000000000000500620202237914    박주영              0000008800100024180210            하이캐피탈          0000000000              00              00              00              00              00               00000                                                  "
+                                          , "06040600000920032902000052003290220032902 30200110000020111124125049007851720000009880                                                                                                                                                                                                      000000              0000003000000 000000000000000000000000000000000400219240194161    최홍길              0000008800100024180210            하이캐피탈          0000000000              00              00              00              00              00               00000                                                  "
 	};
-	String[] sd02_rd_kb_지급이체_KIB전송 = { ""
-                                          , ""
-	};
-	String[] sd03_kb_rd_지급이체_KIB수신 = { ""
-                                          , ""
-	};
-	String[] sd04_지급이체              = { "06040600000920032902000062003290220032902 3021011004352011112318005900782822000000988620032901                                                                                                                                     0 004456642      개시전문 수신전 상태                    000000U114          0000004000000 000000000000000000000000000000000500620202237914    박주영              0000008800100024180210            하이캐피탈          000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000               00000                                                  "
-                                          , ""
+//	String[] sd02_rd_kb_지급이체_KIB전송 = { ""
+//                                          , ""
+//	};
+//	String[] sd03_kb_rd_지급이체_KIB수신 = { ""
+//                                          , ""
+//	};
+	String[] sd04_지급이체              = { "06040600000920032902000062003290220032902 3021011000002011112310032800782822000000988620032901                                                                                                                                     0 009412632      정상처리                                000000              0000004000000 000053464525100000000000000000000500620202237914    박주영              0000008800100024180210            하이캐피탈          002000000000000000000000000000000000000000000000000000000000000000000000000000000000000000               00000                                                  "
+                                          , "06040600000920032902000062003290220032902 3021011000002011112412512800785172000000988620032901                                                                                                                                     0 003743888      정상처리                                000000              0000003000000 000043439525100000000000000000000400219240194161    최홍길              0000008800100024180210            하이캐피탈          002000000000000000000000000000000000000000000000000000000000000000000000000000000000000000               00000                                                  "
 };
 
 	@BeforeClass
@@ -84,102 +92,25 @@ public class SendDaemonTest {
 	public void tearDown() throws Exception {
 	}
 
-	@SuppressWarnings("unused")
-	private static void openSocket(String _ip, int _port) {
-		setSocket(_ip, _port);
-		
-		openSocket();
-	}
-
-	private static void setSocket(String _ip, int _port) {
-		ip = _ip;
-		port = _port;
-	}
-	
-	private static void openSocket() {
-		try {
-			socket = new Socket(ip, port);
-			in  = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-			fail("socket..");
-		} catch (IOException e) {
-			e.printStackTrace();
-			fail("socket open..");
-		}			
-	}
-	
-	private static void closeSocket() {
-		try {
-			in.close();
-			out.close();
-			socket.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-			fail("socket close..");
-		}
-	}
-	
-	private StringBuffer 전문수신() {
-		StringBuffer rtnStr = new StringBuffer("");
-		try {
-			int response;
-			int i = 0;
-			int totCnt = 0;
-			char[] cTot = new char[4];
-			
-			if (in.read(cTot) != -1) {
-				rtnStr.append(new String(cTot));
-				totCnt = StringUtil.cInt(StringUtil.CHSTRING(rtnStr.toString(), " ", "0"));
-				// KIB는 전문길이가 전문에 포함되므로 이미꺼낸 4 byte 는 빼고 꺼낸다.
-				for (i = 0; i < (totCnt - 4); i++) {
-					response = in.read();
-					if (response > 255) {
-						i++;
-					}
-					rtnStr.append((char) response);
-				}
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-			fail("Error Socket Write..");
-		}
-		return rtnStr;
-	}
-
-	private void 전문전송(String 전문) {
-		try {
-			out.write(전문);
-			out.flush();
-		} catch (IOException e) {
-			e.printStackTrace();
-			fail("Error Socket Write..");
-		}
-	}
 
 	@Test
 	public void testSendDaemonSendMsg() {
 		String 보낼전송전문;
 		String 예상수신전문;
-		StringBuffer 받은전문;
 		String 실제전문;
 
 		//////////////////////////////////////////////////////////////////////
 		//////////////////////////////////////////////////////////////////////
-//		setSocket(sdTestIP, sdDevPort);
-		setSocket(localTestIP, sdDevPort);
+		보낼전송전문 = sd01_지급이체[상수_지급이체테스트_20111124_01];
+		예상수신전문 = sd04_지급이체[상수_지급이체테스트_20111124_01];
 		
-		//////////////////////////////////////////////////////////////////////
-		//////////////////////////////////////////////////////////////////////
-		openSocket();
-		보낼전송전문 = sd01_지급이체[상수_지급이체테스트];
-		예상수신전문 = sd04_지급이체[상수_지급이체테스트];
-		전문전송(보낼전송전문);
-		받은전문 = 전문수신();
-		실제전문 = 받은전문.toString();
+		try {
+			실제전문 = GramExecutor.getInstance().sendMsg(GRAM_CD, 보낼전송전문);
+			assertEquals(예상수신전문, 실제전문);
+		} catch (Exception e) {
+			fail("전문 송수신 실패..");
+			e.printStackTrace();
+		}
 		
-		assertEquals(예상수신전문, 실제전문);
-		closeSocket();
 	}
 }
