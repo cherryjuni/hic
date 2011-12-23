6b6e7171a2debcd3b7630ae02f28bbabdbe83126
Alex Yang <movingmt@gmail.com>
2011-12-23 22:35:12 +0900

    쿼리 추가

--- sql/SLNPM0301/펌뱅킹_테스트시점검쿼리.sql
+++ sql/SLNPM0301/펌뱅킹_테스트시점검쿼리.sql
@@ -68,3 +68,47 @@ where
 
 --commit;
 
+
+select * from auser.alot_loan_base
+where img_acnt_no = '56201554000203'
+;
+
+-- 원거래 조회
+SELECT
+--	SETL_FG           
+    *
+FROM BUSER.BVAT_CMS_IAMT_DESC     
+WHERE TR_ORG_CD = 'C1004' AND TR_NO = '201108110342711' --?
+;
+
+'201112230342713'
+'201112230342713'
+;
+
+
+--
+SELECT                                                    
+    CLAS1                                    DEPT_CD,     
+    CLAS2                                    EMP_NO       
+FROM GUSER.GBCT_COMM_CD_DESC                              
+WHERE CD_KIND_NO = 'R00038' --?   /*R00038*/                         
+  AND CD_DESC_NO = '1' --?   /*'1'      */                      
+;
+
+
+-- 원거래 취소
+        "UPDATE BUSER.BVAT_CMS_IAMT_DESC   \n" +
+        "SET                               \n" +
+        "    ORG_TR_DT = ?,                \n" +
+        "    ORG_TR_SNUM = ?,              \n" +
+        "    CNCL_RECV_DT = ?,             \n" +
+        "    CNCL_RECV_TM =?,              \n" +
+        "    CNCL_FG = ?,                  \n" +
+        "    CNCL_DT =?,                   \n" +
+		"    LAST_PROC_DT=?,               \n" +
+		"    LAST_PROC_TM=?,               \n" +
+		"    LAST_PROC_EMP_NO=?            \n" +
+        "WHERE                             \n" +
+        "    TR_ORG_CD = ? AND TR_NO = ?   \n";
+
+
